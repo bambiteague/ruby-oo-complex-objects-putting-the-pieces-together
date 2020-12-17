97effce4dc651b429a8f7562069df988e196a972
bambi teague <teeegsa@gmail.com>
2020-12-16 18:17:10 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,23 +1,15 @@
 # Make your shoe class here!
 class Shoe
-    attr_accessor(:color, :size, :material, :condition)
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :new, :brand
 
     def initialize(new)
         @new = new
     end
 
-    def new
-        @new
-    end
-
     def initialize(brand)
         @brand = brand
     end
-
-    def brand
-        @brand
-    end
-
 
    def cobble
     @condition = "new"  # <---- put inside of the cobble method because it describes the condition of the shoe after its cobbled! 
